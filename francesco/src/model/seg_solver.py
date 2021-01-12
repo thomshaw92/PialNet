Fix bug + different normalization

francesco/src/model/seg_solver.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 import copy
 
 from tf_utils import Tensorboard, MetricsManager, misc
@@ -46,12 +45,11 @@ class Solver:
             y = tf.keras.utils.to_categorical(tf.cast(batch["y"], tf.int32), self.params["out_ch"])
 
             if "train" in mode:
-                predictions, metrics = self.train_step(network, batch["x"], y)
+                predictions, metrics = self.train_step(network, batch["x"], y, mode)
             else:
-                predictions, metrics = self.test_step(network, batch["x"], y)
+                predictions, metrics = self.test_step(network, batch["x"], y, mode)
 
-            if "train" in mode or "val" in mode:
-                self.tb_manager.update_metrics(metrics)
+            self.tb_manager.update_metrics(metrics)
 
         epoch_metrics = self.tb_manager.get_current_metrics()
         self.tb_manager.write_summary(mode, n_epoch, {"x": batch["x"], "y": tf.expand_dims(batch["y"], -1), "pred": predictions})
@@ -62,10 +60,10 @@ class Solver:
         return epoch_metrics
 
     @tf.function
-    def train_step(self, network, x, y):
+    def train_step(self, network, x, y, mode):
 
         with tf.GradientTape() as tape:
-            logits = network(x, training=True)
+            logits = network(x, training=True, mode=mode)
             dice_scores_by_class = self.loss_manager.dice_score_from_logits(y, logits)
             loss = self.loss_manager.metrics[self.params["loss"]](y, logits)
 
@@ -75,8 +73,8 @@ class Solver:
         return misc.get_argmax_prediction(logits), {"loss": loss, "dice_score_by_class": dice_scores_by_class}
 
     @tf.function
-    def test_step(self, network, x, y):
-        logits = network(x, training=False)
+    def test_step(self, network, x, y, mode):
+        logits = network(x, training=False, mode=mode)
 
         if y is None:
             return misc.get_argmax_prediction(logits)

francesco/src/train_seg.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
-from tf_utils import UNet, TFRecordsManager, misc
-from model import seg_solver
+from tf_utils import TFRecordsManager, misc
+from model import seg_solver, unet
 
 
 def main(params):
@@ -13,13 +13,17 @@ def main(params):
     records_manager = TFRecordsManager()
     params["out_ch"] = len(list(range(2)))
     datasets = records_manager.load_datasets(data_path, params["batch_size"])
+    modes = list(datasets.keys())
 
     # Model
-    network = UNet(params["out_ch"], params["n_layers"], params["starting_filters"], params["k_size"], params["kernel_initializer"], params["batch_norm"], params["dropout"],
-                   tf.keras.layers.LeakyReLU, params["conv_per_layer"], params["max_pool"], params["upsampling"], params["kernel_regularizer"], None)
-    #network.summary((128, 128, 128, 1))
+    network = unet.UNet(params["out_ch"], params["n_layers"], params["starting_filters"], params["k_size"], params["kernel_initializer"], params["batch_norm"],
+                        params["dropout"], tf.keras.layers.LeakyReLU, params["conv_per_layer"], params["max_pool"], params["upsampling"],
+                        params["kernel_regularizer"], modes)
+    for mode in modes:
+        network.normalizers[mode].adapt(datasets[mode].map(lambda x: x["x"]))
+        # network.summary((128, 128, 128, 1), mode)
 
-    slv = seg_solver.Solver(ckp_path, params, list(datasets.keys()))
+    slv = seg_solver.Solver(ckp_path, params, modes)
     for n_epoch in range(1000000):
         for mode in datasets:
             slv.iterate_dataset(network, datasets[mode], mode, n_epoch)
@@ -49,7 +53,7 @@ if __name__ == "__main__":
     kernel_initializer_vector = ["he_normal"]
     kernel_regularizer_vector = [None]
     early_stopping = 25
-    data_path = "PialNet_data/TF_records_20210112_141733/"
+    data_path = "PialNet_data/TF_records_20210112_231535/"
 
     for n_layers in n_layers_vector:
         for lr in lr_vector: