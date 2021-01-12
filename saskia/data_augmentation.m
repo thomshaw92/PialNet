280896b0b9d2a1b88c27cca228644e41ee5e12ac
uqsboll2 <uqsboll2@gpunode-2-0.localdomain>
2021-01-12 20:45:00 +1000

    Now loops over all files. Noise might be a bit high.

--- saskia/data_augmentation.m
+++ saskia/data_augmentation.m
@@ -1,34 +1,59 @@
 %% load data
 % load
-orig_data = MrImage('/afm02/Q3/Q3503/synthetic/aug/data_1.nii');
-orig_seg = MrImage('/afm02/Q3/Q3503/synthetic/aug/seg_1.nii');
-% plot and check
-orig_data.plot('z', 27);
-orig_data.plot('z', 27, 'overlayImages', orig_seg, 'overlayMode', 'edge');
+data_path = '/scratch/cvl/uqsboll2/temp/';
+data_folder = 'raw';
+seg_folder = 'seg';
+save_data_folder = 'aug';
+save_seg_folder = 'aug_seg';
 
-%% downsample
-% downsample
-for n = 1:orig_data.dimInfo.nDims
-    new_samplingPoints{n} = orig_data.dimInfo.samplingPoints{n}(1:2:end);
+verbose = 0;
+
+filenames = dir(fullfile(data_path, data_folder, '*.nii'));
+for n = 1:numel(filenames)
+    disp(n);
+    orig_data = MrImage(fullfile(data_path, data_folder, filenames(n).name));
+    orig_seg = MrImage(fullfile(data_path, seg_folder, filenames(n).name));
+    % plot and check
+    if verbose
+        orig_data.plot('z', 27);
+        orig_data.plot('z', 27, 'overlayImages', orig_seg, 'overlayMode', 'edge');
+    end
+    %% downsample
+    % downsample
+    for nD = 1:orig_data.dimInfo.nDims
+        new_samplingPoints{nD} = orig_data.dimInfo.samplingPoints{nD}(1:2:end);
+    end
+    new_dimInfo = MrDimInfo('samplingPoints', new_samplingPoints);
+    down_data = orig_data.resize(new_dimInfo);
+    down_seg = orig_seg.resize(new_dimInfo).binarize(0.2);
+    % plot and check
+    if verbose
+        down_data.plot('z', 13, 'displayRange', [1 255]);
+        down_data.plot('z', 13, 'overlayImages', down_seg, 'displayRange', [1 255]);
+    end
+    %% add lower intensity outline
+    down_seg_outline = down_seg - down_seg.imerode(strel('sphere', 1), '3d');
+    down_data_outline = down_data - down_seg_outline.*down_data.*0.2;
+    
+    if verbose
+        down_data_outline.plot('z', 13, 'displayRange', [1 255]);
+    end
+    %% add more noise
+    noise_image = MrImage(randn(down_data_outline.dimInfo.nSamples).*50);
+    noise_data = down_data_outline + noise_image.threshold(0);
+    if verbose
+        noise_data.plot('z', 13);
+        noise_data.plot('z', 13, 'overlayImages', down_seg, 'overlayMode', 'edge');
+    end
+    
+    %% save
+    noise_data.parameters.save.path = fullfile(data_path, save_data_folder);
+    noise_data.parameters.save.fileName = filenames(n).name;
+    noise_data.save();
+    
+    down_seg.parameters.save.path = fullfile(data_path, save_seg_folder);
+    down_seg.parameters.save.fileName = filenames(n).name;
+    down_seg.save();
+    
+    close all;
 end
-new_dimInfo = MrDimInfo('samplingPoints', new_samplingPoints);
-down_data = orig_data.resize(new_dimInfo);
-down_seg = orig_seg.resize(new_dimInfo).binarize(0.2);
-% plot and check
-down_data.plot('z', 13, 'displayRange', [1 255]);
-down_data.plot('z', 13, 'overlayImages', down_seg, 'displayRange', [1 255]);
-
-%% add lower intensity outline
-down_seg_outline = down_seg - down_seg.imerode(strel('sphere', 1), '3d');
-down_data_outline = down_data - down_seg_outline.*down_data.*0.2;
-
-down_data_outline.plot('z', 13, 'displayRange', [1 255]);
-
-
-%% add more noise
-noise_image = MrImage(randn(down_data_outline.dimInfo.nSamples).*50);
-noise_data = down_data_outline + noise_image.threshold(0);
-noise_data.plot('z', 13);
-noise_data.plot('z', 13, 'overlayImages', down_seg, 'overlayMode', 'edge');
-
-noise_data.parameters.save.path = '/scratch/cvl/uqsboll2/temp';
