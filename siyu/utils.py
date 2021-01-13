4c87f6a8093e543b8b21ad2846a99cbfcb2a2f7f
uqsliu23 <uqsliu23@wiener.hpc.dc.uq.edu.au>
2021-01-13 15:18:29 +1000

    histogram data

--- siyu/utils.py
+++ siyu/utils.py
@@ -14,7 +14,7 @@ def force_create_empty_directory(path):
     shutil.rmtree(path, ignore_errors=True)
     os.makedirs(path)
 
-def get_patch(img, seg, size=50):
+def get_patch(img, seg, size=50, norm=True):
     def rand_seg(d, l):
         lower = 0
         upper = d - l
@@ -29,11 +29,11 @@ def get_patch(img, seg, size=50):
     imgp, segp = img[l1:u1, l2:u2, l3:u3], seg[l1:u1, l2:u2, l3:u3]
     imgp = imgp[None, ...]
     segp = segp[None, ...]
-    return (imgp - imgp.mean()) / imgp.std(), segp
+    if norm:
+        return (imgp - imgp.mean()) / imgp.std(), segp
+    else:
+        return imgp, segp
 
-
-DATA = '/afm02/Q3/Q3503/synthetic/raw'
-SEG = '/afm02/Q3/Q3503/synthetic/seg'
 
 def data_gen(data_dir, seg_dir, images_per_batch=1, patches_per_img=4, patch_size=50):
     data_files = os.listdir(data_dir)
@@ -68,3 +68,33 @@ def save_as_nifti(data, folder, name, affine=np.eye(4)):
     if not os.path.exists(folder):
         os.mkdir(folder)
     nib.save(img, os.path.join(folder, name))
+
+def data_gen_hist(data_dir, seg_dir, images_per_batch=1, patches_per_img=4, patch_size=50, norm=False):
+    data_files = os.listdir(data_dir)
+    cases = {}
+    for f in data_files:
+        if 'nii.gz' not in f:
+            continue
+        case_num = scanf.scanf('%s_%s_%d.nii.gz', f)[2]
+        cases[case_num] = [os.path.join(data_dir, f)]
+
+    seg_files = os.listdir(seg_dir)
+    for f in seg_files:
+        case_num = scanf.scanf('%d.nii.gz', f)[0]
+        cases[case_num].append(os.path.join(seg_dir, f))
+        assert len(cases[case_num]) == 2
+    
+    data = list(cases.values())
+    while True:
+        batch = random.choices(data, k=images_per_batch)
+        xb, yb = [], []
+        for x, y in batch:
+            x = nib.load(x).get_fdata()
+            y = nib.load(y).get_fdata()
+            for i in range(patches_per_img):         
+                xp, yp = get_patch(x, y, size=patch_size, norm=norm)
+                xb.append(xp)
+                yb.append(yp)
+        xb = np.array(xb).astype('float32')
+        yb = np.array(yb)
+        yield torch.tensor(xb).cuda(), torch.tensor(yb).cuda()
