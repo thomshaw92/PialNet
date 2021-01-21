a041e36dff8b3b09b816ab58dcfc581cbe5274d3
vaibhavi5 <vaibhavi.itkyal@gmail.com>
2021-01-21 09:22:10 +0530

    First commit

---
+++ vaibhavi/slurm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --job-name=PialNet
+#SBATCH -n 1
+#SBATCH -c 6
+#SBATCH --mem=50000
+#SBATCH -o out_wiener.txt
+#SBATCH -e error_wiener.txt
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:tesla-smx2:1
+
+module load cuda/10.0.130
+module load gnu7
+module load openmpi3
+module load anaconda/3.6
+source activate /opt/ohpc/pub/apps/tensorflow_2.0.0
+
+srun -n 1 python3 vaibhavi/main.py
