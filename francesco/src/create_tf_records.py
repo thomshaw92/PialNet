6fd91259910059616a646e292f755ac8967eeb88
fracogno <francesco.cognolato@hotmail.com>
2021-02-01 13:30:16 +1000

    TFRecords not augmented

--- francesco/src/create_tf_records.py
+++ francesco/src/create_tf_records.py
@@ -1,4 +1,4 @@
 from preprocessing import data_loader
 
-# data_loader.create_TF_records("PialNet_data/")
-data_loader.create_TF_records_augmented_data("PialNet_data/")
+data_loader.create_TF_records("PialNet_data/")
+# data_loader.create_TF_records_augmented_data("PialNet_data/")
