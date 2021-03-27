48a0d13e7375ffa0b3ae9852633fa97e0859d497
fracogno <francesco.cognolato@hotmail.com>
2021-03-27 22:02:42 +1000

    Normalization testing data matching with training

--- francesco/src/create_tf_records.py
+++ francesco/src/create_tf_records.py
@@ -1,4 +1,3 @@
 from preprocessing import data_loader
 
-data_loader.create_TF_records("PialNet_data/")
-# data_loader.create_TF_records_augmented_data("PialNet_data/")
+data_loader.create_TF_records("dataset/original/")
--- francesco/src/preprocessing/data_loader.py
+++ francesco/src/preprocessing/data_loader.py
@@ -1,6 +1,9 @@
 import glob
 import nibabel as nib
 import numpy as np
+import os
+from intensity_normalization.normalize import nyul
+from intensity_normalization.utilities import io
 
 from tf_utils import misc, TFRecordsManager
 
@@ -9,93 +12,47 @@ def create_TF_records(data_path):
     base_path = misc.get_base_path(training=False)
 
     TFManager = TFRecordsManager()
-    data_purposes = ["train", "val"]
-    data_purposes += ["test-" + str(threshold) for threshold in range(400, 1000, 100)]
+    data_purposes = ["train", "val", "test"]
     params = {"data_purposes": data_purposes, "data_keys": {"x": "float32", "y": "float32"}}
-
     TFRecords_path = misc.create_TF_records_folder(base_path + data_path, data_purposes)
     misc.save_json(TFRecords_path + "params.json", params)
 
+    # Train normalizer to match training data
+    ref_files = glob.glob(base_path + data_path + "train/raw/*")
+    mask_files = [None] * len(ref_files)
+    standard_scale, percs = nyul.train(ref_files, mask_files)
+    misc.save_pickle(TFRecords_path + "norm.pkl", {"standard_scale": standard_scale, "percs": percs})
+
     for data_purpose in data_purposes:
-        if "test" not in data_purpose:
-            x_files = glob.glob(base_path + data_path + data_purpose + "/raw/*")
-            y_files = glob.glob(base_path + data_path + data_purpose + "/seg/*")
-        else:
-            x_files = glob.glob(base_path + data_path + "test/raw/*")
-            y_files = glob.glob(base_path + data_path + "test/seg/*")
+        x_files = glob.glob(base_path + data_path + data_purpose + "/raw/*")
+        y_files = glob.glob(base_path + data_path + data_purpose + "/seg/*")
         x_files.sort()
         y_files.sort()
         assert (len(x_files) == len(y_files))
         data = []
         for i in range(len(x_files)):
-            x = nib.load(x_files[i]).get_fdata()
-            y = nib.load(y_files[i]).get_fdata()
 
-            if "train" == data_purpose or "val" == data_purpose:
-                assert (x.max() == 255. and x.min() == 0. and y.max() == 1.0 and y.min() == 0.)
+            if "train" in data_purpose or "val" in data_purpose:
+                x = nib.load(x_files[i]).get_fdata()
+                y = nib.load(y_files[i]).get_fdata()
+                assert (x.max() <= 255. and x.min() >= 0. and x.shape == y.shape and y.max() == 1 and y.min() == 0. and x.shape == (325, 304, 600))
                 x = x / 255.
-                assert (x.max() == 1. and x.min() == 0.)
 
                 x_patches = misc.make_patches(x, [(29, 30), (40, 40), (20, 20)], 128)
                 y_patches = misc.make_patches(y, [(29, 30), (40, 40), (20, 20)], 128)
                 assert (len(x_patches) == len(y_patches))
 
                 for k in range(len(x_patches)):
+                    if np.count_nonzero(y_patches[k]) == 0.:
+                        continue
                     data.append({"x": np.float32(np.expand_dims(x_patches[k], -1)), "y": np.float32(y_patches[k])})
 
             elif "test" in data_purpose:
-                x = np.pad(x, [(28, 27), (3, 2), (38, 38)], 'constant')
-                y = np.pad(y, [(28, 27), (3, 2), (38, 38)], 'constant')
-                data.append({"x": np.float32(np.expand_dims((np.copy(x) / float(data_purpose.split("-")[1])), -1)), "y": np.float32(y)})
-            else:
-                raise NotImplementedError(data_purpose)
-
-        TFManager.save_record(TFRecords_path + data_purpose + "/0", data)
-        del data
-
-
-def create_TF_records_augmented_data(data_path):
-    base_path = misc.get_base_path(training=False)
-
-    TFManager = TFRecordsManager()
-    data_purposes = ["train_aug", "val_aug"]
-    data_purposes += ["test-" + str(threshold) for threshold in range(400, 1000, 100)]
-    params = {"data_purposes": data_purposes, "data_keys": {"x": "float32", "y": "float32"}}
-
-    TFRecords_path = misc.create_TF_records_folder(base_path + data_path, data_purposes)
-    misc.save_json(TFRecords_path + "params.json", params)
-
-    for data_purpose in data_purposes:
-        if "test" not in data_purpose:
-            x_files = glob.glob(base_path + data_path + data_purpose + "/aug/*")
-            y_files = glob.glob(base_path + data_path + data_purpose + "/aug_seg/*")
-        else:
-            x_files = glob.glob(base_path + data_path + "test/raw/*")
-            y_files = glob.glob(base_path + data_path + "test/seg/*")
-        x_files.sort()
-        y_files.sort()
-        assert (len(x_files) == len(y_files))
-        data = []
-        for i in range(len(x_files)):
-            x = nib.load(x_files[i]).get_fdata()
-            y = nib.load(y_files[i]).get_fdata()
-
-            if "train" in data_purpose or "val" in data_purpose:
-                assert (x.shape[0] == 163 and x.shape[1] == 152 and x.shape[2] == 300)
-                assert (y.shape[0] == 163 and y.shape[1] == 152 and y.shape[2] == 300)
-                x = x / 255.
-
-                x_patches = misc.make_patches(x, [(47, 46), (52, 52), (42, 42)], 128)
-                y_patches = misc.make_patches(y, [(47, 46), (52, 52), (42, 42)], 128)
-                assert (len(x_patches) == len(y_patches))
-
-                for k in range(len(x_patches)):
-                    data.append({"x": np.float32(np.expand_dims(x_patches[k], -1)), "y": np.float32(y_patches[k])})
-
-            elif "test" in data_purpose:
-                x = np.pad(x, [(28, 27), (3, 2), (38, 38)], 'constant')
-                y = np.pad(y, [(28, 27), (3, 2), (38, 38)], 'constant')
-                data.append({"x": np.float32(np.expand_dims((np.copy(x) / float(data_purpose.split("-")[1])), -1)), "y": np.float32(y)})
+                x_normalized = nyul.do_hist_norm(io.open_nii(x_files[i]), percs, standard_scale, mask=None)
+                io.save_nii(x_normalized, TFRecords_path + "normalized.nii", is_nii=True)
+                x_normalized = np.pad(x_normalized.get_fdata() / float(255.), [(95, 95), (1, 2), (16, 16)], 'constant')
+                y = np.pad(nib.load(y_files[i]).get_fdata(), [(95, 95), (1, 2), (16, 16)], 'constant')
+                data.append({"x": np.float32(np.expand_dims(x_normalized, -1)), "y": np.float32(y)})
             else:
                 raise NotImplementedError(data_purpose)
 
