21c44672bb5f85f9d0773e79c09b392ebef9c3f5
thomshaw92 <thomasbasilshaw@gmail.com>
2021-04-12 11:42:25 +1000

    fixes to 160 portions of script downsampling etc

--- tom/histogram_match_by_class.sh
+++ tom/histogram_match_by_class.sh
@@ -29,29 +29,30 @@ ${reference_image_folder}/manual_seg_sub_mask.nii.gz
 
 #then, do the same for all of the new synthetic data - multiply them by their masks (at 160 micron and at native)
 for x in {1..100} ; do
-    fslmaths ${source_image_folder}/${x}.nii.gz -mul ${source_seg_folder}/${x}.nii.gz ${output_dir}/${x}_vessels_only.nii.gz
-    #fslmaths ${source_image_folder_160}/${x}.nii.gz -mul ${source_seg_folder_160}/${x}.nii.gz ${output_dir_160}/${x}_vessels_only.nii.gz
+    #fslmaths ${source_image_folder}/${x}.nii.gz -mul ${source_seg_folder}/${x}.nii.gz ${output_dir}/${x}_vessels_only.nii.gz
+    fslmaths ${source_image_folder_160}/${x}.nii.gz -mul ${source_seg_folder_160}/${x}.nii.gz ${output_dir_160}/${x}_vessels_only.nii.gz
 done
 #and subtract for all of the new synthetic data - sub them by their masks (at 160 micron and at native)
 for x in {1..100} ; do
-    fslmaths ${source_image_folder}/${x}.nii.gz -sub ${source_seg_folder}/${x}.nii.gz ${output_dir}/${x}_brain_only.nii.gz
-    #fslmaths ${source_image_folder_160}/${x}.nii.gz -sub ${source_seg_folder_160}/${x}.nii.gz ${output_dir_160}/${x}_brain_only.nii.gz
+    #fslmaths ${source_image_folder}/${x}.nii.gz -sub ${source_seg_folder}/${x}.nii.gz ${output_dir}/${x}_brain_only.nii.gz
+    fslmaths ${source_image_folder_160}/${x}.nii.gz -sub ${source_seg_folder_160}/${x}.nii.gz ${output_dir_160}/${x}_brain_only.nii.gz
 done
 #then histmatch the seg class synthetic to the seg class manual,
 for x in {1..100} ; do
-    ImageMath 3 ${output_dir}/${x}_segmentation_class_histmatched.nii.gz HistogramMatch ${output_dir}/${x}_vessels_only.nii.gz ${reference_image_folder}/manual_seg_mult_mask.nii.gz
-    #ImageMath 3 ${output_dir}/${x}_segmentation_class_histmatched_160.nii.gz HistogramMatch ${output_dir}/${x}_vessels_only.nii.gz ${reference_image_folder}/manual_seg_mult_mask.nii.gz
+    #ImageMath 3 ${output_dir}/${x}_segmentation_class_histmatched.nii.gz HistogramMatch ${output_dir}/${x}_vessels_only.nii.gz ${reference_image_folder}/manual_seg_mult_mask.nii.gz
+    ImageMath 3 ${output_dir_160}/${x}_segmentation_class_histmatched_160.nii.gz HistogramMatch ${output_dir_160}/${x}_vessels_only.nii.gz ${reference_image_folder}/manual_seg_mult_mask.nii.gz
 done
 #and the synth brain to the manual brain
 for x in {1..100} ; do
-    ImageMath 3 ${output_dir}/${x}_brain_class_histmatched.nii.gz HistogramMatch ${output_dir}/${x}_brain_only.nii.gz ${reference_image_folder}/manual_seg_sub_mask.nii.gz
-    #ImageMath 3 ${output_dir}/${x}_brain_class_histmatched_160.nii.gz HistogramMatch ${output_dir}/${x}_brain_only.nii.gz ${reference_image_folder}/manual_seg_sub_mask.nii.gz
+    #ImageMath 3 ${output_dir}/${x}_brain_class_histmatched.nii.gz HistogramMatch ${output_dir}/${x}_brain_only.nii.gz ${reference_image_folder}/manual_seg_sub_mask.nii.gz
+    ImageMath 3 ${output_dir_160}/${x}_brain_class_histmatched_160.nii.gz HistogramMatch ${output_dir_160}/${x}_brain_only.nii.gz ${reference_image_folder}/manual_seg_sub_mask.nii.gz
 done
 #add the new histmatched classes back together to make new images
 for x in {1..100} ; do
-    fslmaths ${output_dir}/${x}_segmentation_class_histmatched.nii.gz -add ${output_dir}/${x}_brain_class_histmatched.nii.gz ${output_dir}/${x}_both_classes_histmatched.nii.gz
-    #fslmaths ${output_dir_160}/${x}_segmentation_class_histmatched_160.nii.gz -add ${output_dir}/${x}_brain_class_histmatched_160.nii.gz ${output_dir}/${x}_both_classes_histmatched_160.nii.gz
+    #fslmaths ${output_dir}/${x}_segmentation_class_histmatched.nii.gz -add ${output_dir}/${x}_brain_class_histmatched.nii.gz ${output_dir}/${x}_both_classes_histmatched.nii.gz
+    fslmaths ${output_dir_160}/${x}_segmentation_class_histmatched_160.nii.gz -add ${output_dir_160}/${x}_brain_class_histmatched_160.nii.gz ${output_dir_160}/${x}_both_classes_histmatched_160.nii.gz
 done
 for x in {1..100} ; do
-    ImageMath 3 ${output_dir}/${x}_both_classes_histmatched.nii.gz RescaleImage ${output_dir}/${x}_both_classes_histmatched.nii.gz 0 1 
+    #ImageMath 3 ${output_dir}/${x}_both_classes_histmatched.nii.gz RescaleImage ${output_dir}/${x}_both_classes_histmatched.nii.gz 0 1 
+    ImageMath 3 ${output_dir_160}/${x}_both_classes_histmatched_160.nii.gz RescaleImage ${output_dir_160}/${x}_both_classes_histmatched_160.nii.gz 0 1
 done
