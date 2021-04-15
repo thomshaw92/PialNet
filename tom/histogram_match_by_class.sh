748f7bed1d6b292c400dc69e16f1b5fa69cd956f
thomshaw92 <thomasbasilshaw@gmail.com>
2021-04-15 17:24:00 +1000

    code working

--- tom/histogram_match_by_class.sh
+++ tom/histogram_match_by_class.sh
@@ -19,20 +19,20 @@ mkdir -p ${output_dir} ${output_dir_160} ${output_dir_rescaled}
 ml ants
 ml fsl
 # multiply the manual seg by the manual seg mask (seg_TOF_etc) to make seg only
-fslmaths ${reference_image_folder}/TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_denoised_2SR_resampled_22-52_masked.nii \
--mul ${reference_image_folder}/seg_TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_H400_L300_C10_resized_22-52_masked.nii \
-${reference_image_folder}/manual_seg_mult_mask.nii.gz
+#fslmaths ${reference_image_folder}/TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_denoised_2SR_resampled_22-52_masked.nii \
+#-mul ${reference_image_folder}/seg_TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_H400_L300_C10_resized_22-52_masked.nii \
+#${reference_image_folder}/manual_seg_mult_mask.nii.gz
 
 #first replace the seg values of 1s  with 2s
-ImageMath 3 ${reference_image_folder}/seg_inverted.nii.gz ReplaceVoxelValue ${reference_image_folder}/seg_TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_H400_L300_C10_resized_22-52_masked.nii 1 1 2
+#ImageMath 3 ${reference_image_folder}/seg_inverted.nii.gz ReplaceVoxelValue ${reference_image_folder}/seg_TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_H400_L300_C10_resized_22-52_masked.nii 1 1 2
 #then the seg 0s with ones
-ImageMath 3 ${reference_image_folder}/seg_inverted.nii.gz ReplaceVoxelValue ${reference_image_folder}/seg_inverted.nii.gz 0 0 1
+#ImageMath 3 ${reference_image_folder}/seg_inverted.nii.gz ReplaceVoxelValue ${reference_image_folder}/seg_inverted.nii.gz 0 0 1
 #then all the seg 2s with 0s
-ImageMath 3 ${reference_image_folder}/seg_inverted.nii.gz ReplaceVoxelValue ${reference_image_folder}/seg_inverted.nii.gz 2 2 0
+#ImageMath 3 ${reference_image_folder}/seg_inverted.nii.gz ReplaceVoxelValue ${reference_image_folder}/seg_inverted.nii.gz 2 2 0
 #then multiply the new inverse mask manual seg mask by the original to make brain meat only
-fslmaths ${reference_image_folder}/TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_denoised_2SR_resampled_22-52_masked.nii \
--mul ${reference_image_folder}/seg_inverted.nii.gz \
-${reference_image_folder}/manual_seg_sub_mask.nii.gz
+#fslmaths ${reference_image_folder}/TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_denoised_2SR_resampled_22-52_masked.nii \
+#-mul ${reference_image_folder}/seg_inverted.nii.gz \
+#${reference_image_folder}/manual_seg_sub_mask.nii.gz
 
 #then, do the same for all of the new synthetic data - multiply them by their masks (at 160 micron and at native)
 for x in {1..100} ; do
@@ -44,8 +44,8 @@ for x in {1..100} ; do
     echo "brain ${x}"
     #invert mask
     ImageMath 3 ${output_dir}/${x}_seg_inverted.nii.gz ReplaceVoxelValue ${source_seg_folder}/${x}.nii.gz 1 1 2
-    ImageMath 3 ${output_dir}/${x}_seg_inverted.nii.gz ReplaceVoxelValue ${source_seg_folder}/${x}_seg_inverted.nii.gz 0 0 1
-    ImageMath 3 ${output_dir}/${x}_seg_inverted.nii.gz ReplaceVoxelValue ${source_seg_folder}/${x}_seg_inverted.nii.gz 2 2 0
+    ImageMath 3 ${output_dir}/${x}_seg_inverted.nii.gz ReplaceVoxelValue ${output_dir}/${x}_seg_inverted.nii.gz 0 0 1
+    ImageMath 3 ${output_dir}/${x}_seg_inverted.nii.gz ReplaceVoxelValue ${output_dir}/${x}_seg_inverted.nii.gz 2 2 0
     fslmaths ${source_image_folder}/${x}.nii.gz -mul ${output_dir}/${x}_seg_inverted.nii.gz ${output_dir}/${x}_brain_only.nii.gz
 done
 #then histmatch the seg class synthetic to the seg class manual,
