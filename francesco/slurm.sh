8b2e5f01e69f789d5f5ed74cc46d6167a4e9b0e1
fracogno <francesco.cognolato@hotmail.com>
2021-05-19 01:22:30 +1000

    Fix

--- francesco/slurm.sh
+++ francesco/slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH -o out_wiener.txt
 #SBATCH -e error_wiener.txt
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:tesla-smx2:1
+#SBATCH --gres=gpu:1
 
 module load cuda/10.0.130
 module load gnu7
@@ -15,11 +15,10 @@ module load openmpi3
 module load anaconda/3.6
 
 #conda create -y --name lab2im_env python=3.6
-conda env list
+#conda env list
+#source activate lab2im_env
+srun -n 1 python3 francesco/src/train.py
 
-source activate lab2im_env
-# srun -n 1 python3 francesco/src/train.py
-
-input_path="dataset/MRA_P09_denoised/TOF_3D_300um_TR20_TE4p73_sli72_15_biasCor_denoised_5SR.nii.gz"
-srun -n 1 python3 francesco/src/predict.py --ckp_path checkpoints/SEG-ckp_20210328_002027/ --ckp_name test-loss --input_path $input_path
-conda deactivate
+#input_path="dataset/MRA_P09_denoised/TOF_3D_300um_TR20_TE4p73_sli72_15_biasCor_denoised_2SR.nii.gz"
+#srun -n 1 python3 francesco/src/predict.py --ckp_path checkpoints/SEG-ckp_20210328_002027/ --ckp_name test-loss --input_path $input_path
+#conda deactivate
