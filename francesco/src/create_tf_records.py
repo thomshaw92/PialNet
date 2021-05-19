153c7efacaad5292b89dcf443a292780f03bc892
fracogno <francesco.cognolato@hotmail.com>
2021-05-20 01:49:09 +1000

    Half manual seg for training

--- francesco/src/create_tf_records.py
+++ francesco/src/create_tf_records.py
@@ -1,3 +1,4 @@
 from preprocessing import data_loader
 
-data_loader.create_TF_records("dataset/original/", normalize=True)
+#data_loader.create_TF_records("dataset/original/", normalize=True)
+data_loader.create_TF_records_with_half_manual("dataset/original/", normalize=True)
--- francesco/src/preprocessing/data_loader.py
+++ francesco/src/preprocessing/data_loader.py
@@ -39,7 +39,7 @@ def create_TF_records(data_path, normalize):
                     x /= float(255.)
 
                 if "original" in data_path:
-                    assert(x.shape == (325, 304, 600))
+                    assert (x.shape == (325, 304, 600))
                     x_patches = misc.make_patches(x, [(29, 30), (40, 40), (20, 20)], 128)
                     y_patches = misc.make_patches(y, [(29, 30), (40, 40), (20, 20)], 128)
                 assert (len(x_patches) == len(y_patches))
@@ -51,17 +51,95 @@ def create_TF_records(data_path, normalize):
 
             elif "test" in data_purpose:
                 x_normalized = nyul.do_hist_norm(io.open_nii(x_files[i]), percs, standard_scale, mask=None)
-                io.save_nii(x_normalized, TFRecords_path + "normalized.nii", is_nii=True)
+                # io.save_nii(x_normalized, TFRecords_path + "normalized.nii", is_nii=True)
                 x_normalized = x_normalized.get_fdata()
                 if normalize:
                     assert ("original" in data_path)
                     x_normalized /= float(255.)
-                assert(x_normalized.shape == (1090, 1277, 32))
-                x_normalized = np.pad(x_normalized, [(95, 95), (1, 2), (16, 16)], 'constant')
-                y = np.pad(nib.load(y_files[i]).get_fdata(), [(95, 95), (1, 2), (16, 16)], 'constant')
+                assert (x_normalized.shape == (1090, 1277, 32))
+                x_normalized = np.pad(x_normalized, [(95, 95), (1, 2), (0, 0)], 'constant')
+                y = np.pad(nib.load(y_files[i]).get_fdata(), [(95, 95), (1, 2), (0, 0)], 'constant')
                 data.append({"x": np.float32(np.expand_dims(x_normalized, -1)), "y": np.float32(y)})
             else:
                 raise NotImplementedError(data_purpose)
+
+        TFManager.save_record(TFRecords_path + data_purpose + "/0", data)
+        del data
+
+
+def create_TF_records_with_half_manual(data_path, normalize):
+    base_path = misc.get_base_path(training=False)
+
+    TFManager = TFRecordsManager()
+    data_purposes = ["train", "val", "test"]
+    params = {"data_purposes": data_purposes, "data_keys": {"x": "float32", "y": "float32"}, "normalize": normalize, "data_path": data_path}
+    TFRecords_path = misc.create_TF_records_folder(base_path + data_path, data_purposes)
+    misc.save_json(TFRecords_path + "params.json", params)
+
+    # Train normalizer to match training data
+    ref_files = glob.glob(base_path + data_path + "train/raw/*")
+    mask_files = [None] * len(ref_files)
+    standard_scale, percs = nyul.train(ref_files, mask_files)
+    misc.save_pickle(TFRecords_path + "norm.pkl", {"standard_scale": standard_scale, "percs": percs})
+
+    # Normalize test volume and load manual segmentation
+    name = "TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_denoised_2SR_resampled_22-52_masked.nii"
+    x_normalized = nyul.do_hist_norm(io.open_nii(base_path + data_path + "test/raw/" + name), percs, standard_scale, mask=None).get_fdata()
+    x_normalized /= float(255.)
+    assert (x_normalized.shape == (1090, 1277, 32))
+
+    name = "seg_TOF_3D_160um_TR20_TE6p56_sli52_FA18_FCY_BW100_27_biasCor_zipCor_H400_L300_C10_resized_22-52_masked.nii"
+    y_normalized = nib.load(base_path + data_path + "test/seg/" + name).get_fdata()
+
+    # Pad
+    x_normalized = np.pad(x_normalized, [(95, 95), (1, 2), (0, 0)], 'constant')
+    y_normalized = np.pad(y_normalized, [(95, 95), (1, 2), (0, 0)], 'constant')
+
+    # Split first half for training, second for testing
+    x_test_first_half = x_normalized[:, :, :16]
+    x_test_second_half = x_normalized[:, :, 16:]
+    y_test_first_half = y_normalized[:, :, :16]
+    y_test_second_half = y_normalized[:, :, 16:]
+
+    for data_purpose in data_purposes:
+        x_files = glob.glob(base_path + data_path + data_purpose + "/raw/*")
+        y_files = glob.glob(base_path + data_path + data_purpose + "/seg/*")
+        x_files.sort()
+        y_files.sort()
+        assert (len(x_files) == len(y_files))
+        data = []
+        for i in range(len(x_files)):
+
+            if "train" in data_purpose or "val" in data_purpose:
+                x = nib.load(x_files[i]).get_fdata()
+                y = nib.load(y_files[i]).get_fdata()
+                if normalize:
+                    assert (x.max() <= 255. and x.min() >= 0. and x.shape == y.shape and y.max() == 1 and y.min() == 0. and "original" in data_path)
+                    x /= float(255.)
+
+                if "original" in data_path:
+                    assert (x.shape == (325, 304, 600))
+                    x_patches = misc.make_patches(x, [(29, 30), (40, 40), (20, 20)], 128)
+                    y_patches = misc.make_patches(y, [(29, 30), (40, 40), (20, 20)], 128)
+                assert (len(x_patches) == len(y_patches))
+
+                for k in range(len(x_patches)):
+                    if np.count_nonzero(y_patches[k]) == 0.:
+                        continue
+                    data.append({"x": np.float32(np.expand_dims(x_patches[k], -1)), "y": np.float32(y_patches[k])})
+
+            elif "test" in data_purpose:
+                data.append({"x": np.float32(np.expand_dims(x_test_second_half, -1)), "y": np.float32(y_test_second_half)})
+            else:
+                raise NotImplementedError(data_purpose)
+
+        if "train" in data_purpose:
+            x_patches = misc.make_patches(x_test_first_half, [(0, 0), (0, 0), (56, 56)], 128)
+            y_patches = misc.make_patches(x_test_first_half, [(0, 0), (0, 0), (56, 56)], 128)
+            for k in range(len(x_patches)):
+                if np.count_nonzero(y_patches[k]) == 0.:
+                    continue
+                data.append({"x": np.float32(np.expand_dims(x_patches[k], -1)), "y": np.float32(y_patches[k])})
 
         TFManager.save_record(TFRecords_path + data_purpose + "/0", data)
         del data
