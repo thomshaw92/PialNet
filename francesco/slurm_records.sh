794704f67da48b8228402f9befa07c2cb260a0f0
fracogno <francesco.cognolato@hotmail.com>
2021-05-19 10:30:54 +1000

    Fix slurm

--- francesco/slurm_records.sh
+++ francesco/slurm_records.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH --job-name=lab2im
+#SBATCH --job-name=PialNet
 #SBATCH -n 1
 #SBATCH -c 6
 #SBATCH --mem=250000
@@ -9,10 +9,11 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
+module load cuda/10.0.130
+module load gnu7
+module load openmpi3
 module load anaconda/3.6
-#conda create -y --name lab2im_env python=3.6
-conda env list
+source activate /opt/ohpc/pub/apps/tensorflow_2.0.0
 
-source activate lab2im_env
 srun -n 1 python3 francesco/src/create_tf_records.py
-conda deactivate
+
