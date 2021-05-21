4e7d23974c53f9a25a88c10867035a91c24459d6
fracogno <francesco.cognolato@hotmail.com>
2021-05-22 01:59:53 +1000

    Slurm added prediction

--- francesco/slurm.sh
+++ francesco/slurm.sh
@@ -17,5 +17,16 @@ source activate /opt/ohpc/pub/apps/tensorflow_2.0.0
 
 # srun -n 1 python3 francesco/src/create_tf_records.py
 # srun -n 1 python3 francesco/src/train.py
-srun -n 1 python3 francesco/src/predict.py --ckp_path checkpoints/SEG-ckp_20210521_014649/ --ckp_name test-loss --input_path \
-	"dataset/MRA_P09_denoised/TOF_3D_300um_TR20_TE4p73_sli72_15_biasCor_denoised_5SR.nii.gz"
+#srun -n 1 python3 francesco/src/predict.py --ckp_path checkpoints/SEG-ckp_20210521_014649/ --ckp_name test-loss --input_path \
+#	"dataset/MRA_P09_denoised/TOF_3D_300um_TR20_TE4p73_sli72_15_biasCor_denoised_5SR.nii.gz"
+
+
+ckp_folder="checkpoints/"
+data_folder="dataset/MRA_P09_denoised/"
+for ckp in $(ls "francesco/"$ckp_folder)
+do
+	for res in $(ls "francesco/"$data_folder)
+	do
+		python3 francesco/src/predict.py --ckp_path $ckp_folder$ckp --ckp_name test-loss --input_path $data_folder$res
+	done
+done
